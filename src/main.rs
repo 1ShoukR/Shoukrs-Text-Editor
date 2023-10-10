ab261dae23290ac71f4cdef522137429afb2e04e
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-10-10 17:21:30 -0400

    looping over bytes, unrwapping them, assigning the byte to a character, and printing the byte and character in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::{self, Read};
+use std::io::{self, Read, stdout};
+use crossterm::terminal::enable_raw_mode;
 /*
 Short for: 
 use std::io
@@ -7,12 +8,25 @@ use std::io::Read
 
 
 fn main() {
+    let _raw_mode = enable_raw_mode().unwrap();
+
     for byte in io::stdin().bytes() {
-        let character:char = byte.unwrap() as char;
-        println!("{}", character);
-        if character == 'q' { // NOTE: Characters in Rust are single quoted to work, not double quoted
-            // this if statement ends the program if there is a character 'q' in the input
-            break;
+        let byte = byte.unwrap();
+        let character = byte as char;
+        if character.is_control() {
+            println!("{:?} \r", byte);
+        } else {
+            println!("{:?} ({})\r", byte, character);
         }
+
+
+
+
+        // let b = b.unwrap();
+        // let character = b as char;
+        // if character == 'q' { // NOTE: Characters in Rust are single quoted to work, not double quoted
+        //     // this if statement ends the program if there is a character 'q' in the input
+        //     break;
+        // }
     }
 }
