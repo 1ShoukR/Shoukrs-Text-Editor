ce1d922c6665647f5c879358d55d4e6d7fad0daf
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-10-10 11:52:00 -0400

    created inpuit stream loop to read characters inputed by a user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, Read};
+/*
+Short for: 
+use std::io
+use std::io::Read
+*/ 
+
+
 fn main() {
-    println!("Hello, world!");
+    for byte in io::stdin().bytes() {
+        let character:char = byte.unwrap() as char;
+        println!("{}", character);
+        if character == 'q' { // NOTE: Characters in Rust are single quoted to work, not double quoted
+            // this if statement ends the program if there is a character 'q' in the input
+            break;
+        }
+    }
 }
